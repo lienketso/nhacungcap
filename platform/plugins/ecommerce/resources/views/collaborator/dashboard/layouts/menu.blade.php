9315867fed78c83efa6fdbbe6c43deda5390b604
lienketso <thanhan1507@gmail.com>
2024-08-07 16:50:21 +0700

    update menu vendor edit product

--- platform/plugins/ecommerce/resources/views/collaborator/dashboard/layouts/menu.blade.php
+++ platform/plugins/ecommerce/resources/views/collaborator/dashboard/layouts/menu.blade.php
@@ -1,73 +1,26 @@
 @php
     $menus = collect([
         [
-            'key'   => 'collaborator.dashboard',
+            'key'   => 'marketplace.vendor.dashboard',
             'icon'  => 'icon material-icons md-home',
             'name'  => 'Tổng quan',
             'order' => 0,
         ],
-//        [
-//            'key'    => 'collaborator.product.index',
-//            'icon'   => 'icon material-icons md-shopping_bag',
-//            'name'   => __('Products'),
-//            'order' => 1,
-//        ],
         [
-            'key'   => 'collaborator.tree.index',
-            'icon'  => 'icon material-icons md-group',
-            'name'  => 'Hệ thống đại lý',
-            'order' => 2,
+            'key'    => 'marketplace.vendor.products.index',
+            'icon'   => 'icon material-icons md-shopping_bag',
+            'name'   => __('Products'),
+            'order' => 1,
         ],
+
         [
-            'key'   => 'collaborator.customers.index',
-            'icon'  => 'icon material-icons md-group',
-            'name'  => 'Danh sách khách hàng',
-            'order' => 3,
-        ],
-        [
-            'key'   => 'collaborator.orders.index',
-            'icon'  => 'icon material-icons md-shopping_cart',
-            'name'  => 'Đơn hàng Online',
-            'order' => 4,
-        ],
-        [
-            'key'   => 'collaborator.bill.index',
-            'icon'  => 'icon material-icons md-shop',
-            'name'  => 'Đơn hàng tại chỗ',
-            'order' => 5,
-        ],
-        [
-            'key'   => 'collaborator.ban-hang',
-            'icon'  => 'icon material-icons md-monetization_on',
-            'name'  => 'Bán hàng',
-            'target'=>'_blank',
-            'order' => 6,
-        ],
-        [
-            'key'   => 'collaborator.revenues.index',
-            'icon'  => 'icon material-icons md-monetization_on',
-            'name'  => 'Thu nhập',
-            'order' => 6,
-        ],
-        [
-            'key'   => 'collaborator.withdrawals.index',
-            'icon'  => 'icon material-icons md-money',
-            'name'  => 'Rút tiền',
-            'order' => 7,
-            'routes' => [
-                'collaborator.withdrawals.create',
-                'collaborator.withdrawals.edit',
-                'collaborator.withdrawals.show'
-            ],
-        ],
-        [
-            'key'   => 'collaborator.settings',
+            'key'   => 'marketplace.vendor.settings',
             'icon'  => 'icon material-icons md-settings',
             'name'  => 'Cài đặt',
             'order' => 8,
         ],
         [
-            'key'   => 'customer.overview',
+            'key'   => 'marketplace.vendor.dashboard',
             'icon'  => 'icon material-icons md-person',
             'name'  => 'Về giao diện tài khoản',
             'order' => 9,
